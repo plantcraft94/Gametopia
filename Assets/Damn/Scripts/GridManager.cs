de254441774b048b2673e7bb3ea755d13f5747e6
Dam595 <damtheanh595@gmail.com>
2026-02-23 18:07:40 +0700

    ff2

--- Assets/Damn/Scripts/GridManager.cs
+++ Assets/Damn/Scripts/GridManager.cs
@@ -1,73 +1,39 @@
 ﻿using UnityEngine;
-
-public enum CellType
-{
-    Empty,
-    Wall,
-    Goal
-}
+using UnityEngine.Tilemaps;
 
 public class GridManager : MonoBehaviour
 {
-    public int width = 8;
-    public int height = 6;
+    public Tilemap groundTilemap;
+    public Tilemap wallTilemap;
+    public Tilemap goalTilemap;
+
     public float cellSize = 1f;
 
-    public CellType[,] grid;
-
-    void Awake()
+    public Vector2Int WorldToCell(Vector3 worldPos)
     {
-        grid = new CellType[width, height];
-
-        for (int x = 0; x < width; x++)
-        {
-            grid[x, 0] = CellType.Wall;
-            grid[x, height - 1] = CellType.Wall;
-        }
-
-        for (int y = 0; y < height; y++)
-        {
-            grid[0, y] = CellType.Wall;
-            grid[width - 1, y] = CellType.Wall;
-        }
-
-        grid[3, 2] = CellType.Wall;
-
-        grid[6, 3] = CellType.Goal;
-    }
-
-    public Vector2Int WorldToCell(Vector3 pos)
-    {
-        return new Vector2Int(
-            Mathf.RoundToInt(pos.x / cellSize),
-            Mathf.RoundToInt(pos.y / cellSize)
-        );
+        Vector3Int cell = groundTilemap.WorldToCell(worldPos);
+        return new Vector2Int(cell.x, cell.y);
     }
 
     public Vector3 CellToWorld(Vector2Int cell)
     {
-        return new Vector3(
-            cell.x * cellSize,
-            cell.y * cellSize,
-            0
-        );
-    }
-
-    public bool IsInside(Vector2Int cell)
-    {
-        return cell.x >= 0 && cell.x < width &&
-               cell.y >= 0 && cell.y < height;
+        Vector3Int c = new Vector3Int(cell.x, cell.y, 0);
+        return groundTilemap.GetCellCenterWorld(c);
     }
 
     public bool IsWalkable(Vector2Int cell)
     {
-        if (!IsInside(cell)) return false;
-        return grid[cell.x, cell.y] != CellType.Wall;
+        Vector3Int c = new Vector3Int(cell.x, cell.y, 0);
+
+        if (wallTilemap.HasTile(c))
+            return false;
+
+        return true;
     }
 
     public bool IsGoal(Vector2Int cell)
     {
-        if (!IsInside(cell)) return false;
-        return grid[cell.x, cell.y] == CellType.Goal;
+        Vector3Int c = new Vector3Int(cell.x, cell.y, 0);
+        return goalTilemap.HasTile(c);
     }
 }
