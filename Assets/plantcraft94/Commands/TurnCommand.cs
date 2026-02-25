ab96c39e4815a3b8d73cac12034402107a7f2a07
plantcraft94 <plantcraft60@gmail.com>
2026-02-25 16:47:03 +0700

    Add ui for turn command and a new function for choose turn in the turn command script

--- Assets/plantcraft94/Commands/TurnCommand.cs
+++ Assets/plantcraft94/Commands/TurnCommand.cs
@@ -1,9 +1,25 @@
 using System.Collections;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class TurnCommand : MonoBehaviour, IInstruction
 {
     public bool turnRight;
+    public void Choose(int index)
+    {
+        switch (index)
+        {
+            case 0:
+               turnRight = false;
+               break;
+            case 1:
+               turnRight = true;
+               break;
+            default:
+                turnRight = false;
+                break;
+        }
+    }
 
     public IEnumerator RunInstruction(ExecutionContext context)
     {
