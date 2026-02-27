6a67079dd1a3d7235531d1567722f9e433b59bb6
Dam595 <96290770+Dam595@users.noreply.github.com>
2026-02-27 14:20:02 +0700

    them visual cho JUMP

--- Assets/Damn/Scripts/IfVisualConnector.cs
+++ Assets/Damn/Scripts/IfVisualConnector.cs
@@ -7,17 +7,19 @@ public class IfVisualConnector : MonoBehaviour
     public RectTransform endBlock;
 
     IfCommand ifCommand;
+    JumpCommand jumpCommand;
 
     void Awake()
     {
         ifCommand = GetComponent<IfCommand>();
+        jumpCommand = GetComponent<JumpCommand>();
     }
 
     void Update()
     {
-        if (ifCommand.pair == null) return;
-
-        var end = ifCommand.pair.GetComponent<RectTransform>();
+        if (ifCommand.pair == null && jumpCommand.pair == null) return;
+        if(ifCommand != null){
+            var end = ifCommand.pair.GetComponent<RectTransform>();
 
         Vector3 startPos = transform.position;
         Vector3 endPos = end.position;
@@ -29,5 +31,21 @@ public class IfVisualConnector : MonoBehaviour
             startPos.x - 60f,
             (startPos.y + endPos.y) / 2f,
             0);
+        }
+        if(jumpCommand != null){
+            var end = jumpCommand.pair.GetComponent<RectTransform>();
+
+        Vector3 startPos = transform.position;
+        Vector3 endPos = end.position;
+
+        float height = Mathf.Abs(endPos.y - startPos.y);
+
+        lineImage.sizeDelta = new Vector2(4, height);
+        lineImage.position = new Vector3(
+            startPos.x - 60f,
+            (startPos.y + endPos.y) / 2f,
+            0);
+        }
+        
     }
 }
