df5c0a5a9daa633a295e8f2e1a87896272ecb6f7
plantcraft94 <plantcraft60@gmail.com>
2026-02-28 01:32:55 +0700

    Chỉnh lại UI cho VisualConnector của if command

--- Assets/Damn/Scripts/IfVisualConnector.cs
+++ Assets/Damn/Scripts/IfVisualConnector.cs
@@ -5,6 +5,7 @@ public class IfVisualConnector : MonoBehaviour
 {
     public RectTransform lineImage;
     public RectTransform endBlock;
+    RectTransform rect;
 
     IfCommand ifCommand;
     JumpCommand jumpCommand;
@@ -13,39 +14,42 @@ public class IfVisualConnector : MonoBehaviour
     {
         ifCommand = GetComponent<IfCommand>();
         jumpCommand = GetComponent<JumpCommand>();
+        rect = GetComponent<RectTransform>();
     }
 
     void Update()
     {
         if (ifCommand.pair == null && jumpCommand.pair == null) return;
-        if(ifCommand != null){
+        if (ifCommand != null)
+        {
             var end = ifCommand.pair.GetComponent<RectTransform>();
 
-        Vector3 startPos = transform.position;
-        Vector3 endPos = end.position;
+            Vector3 startPos = transform.position;
+            Vector3 endPos = end.position;
 
-        float height = Mathf.Abs(endPos.y - startPos.y);
+            float height = Mathf.Abs(endPos.y - startPos.y) + end.sizeDelta.y + 20;
 
-        lineImage.sizeDelta = new Vector2(4, height);
-        lineImage.position = new Vector3(
-            startPos.x - 60f,
-            (startPos.y + endPos.y) / 2f,
-            0);
+            lineImage.sizeDelta = new Vector2(lineImage.sizeDelta.x, height);
+            // lineImage.position = new Vector3(
+            //     lineImage.rect.position.x,
+            //     (startPos.y + endPos.y) / 2f,
+            //     0);
         }
-        if(jumpCommand != null){
+        if (jumpCommand != null)
+        {
             var end = jumpCommand.pair.GetComponent<RectTransform>();
 
-        Vector3 startPos = transform.position;
-        Vector3 endPos = end.position;
+            Vector3 startPos = transform.position;
+            Vector3 endPos = end.position;
 
-        float height = Mathf.Abs(endPos.y - startPos.y);
+            float height = Mathf.Abs(endPos.y - startPos.y);
 
-        lineImage.sizeDelta = new Vector2(4, height);
-        lineImage.position = new Vector3(
-            startPos.x - 60f,
-            (startPos.y + endPos.y) / 2f,
-            0);
+            lineImage.sizeDelta = new Vector2(4, height);
+            lineImage.position = new Vector3(
+                startPos.x - 60f,
+                (startPos.y + endPos.y) / 2f,
+                0);
         }
-        
+
     }
 }
